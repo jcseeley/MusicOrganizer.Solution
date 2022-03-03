85034f1efee41760d5d9e44adad4cbfa2241faa7
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-03 12:20:37 -0800

    add clear all method and code to pass GetAll tests.

--- MusicOrganizer.Tests/ModelTests/RecordTests.cs
+++ MusicOrganizer.Tests/ModelTests/RecordTests.cs
@@ -6,8 +6,12 @@ using MusicOrganizer.Models;
 namespace MusicOrganizer.Tests
 {
   [TestClass]
-  public class RecordTests
+  public class RecordTests : IDisposable
   {
+    public void Dispose()
+    {
+      Record.ClearAll();
+    }
     
     [TestMethod]
     public void RecordConstructor_CreatesInstanceOfRecord_Object()
@@ -23,6 +27,26 @@ namespace MusicOrganizer.Tests
       Record newRecord = new Record(newAlbum);
       string result = newRecord.AlbumTitle;
       Assert.AreEqual(newAlbum, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_RecordList()
+    {
+      List<Record> newList = new List<Record> { };
+      List<Record> result = Record.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+    
+    [TestMethod]
+    public void GetAll_ReturnsAllRecords_RecordList()
+    {
+      string newAlbum = "Test";
+      string newAlbum2 = "Test2";
+      Record newRecord = new Record(newAlbum);
+      Record newRecord2 = new Record(newAlbum2);
+      List<Record> newList = new List<Record> { newRecord, newRecord2 };
+      List<Record> result = Record.GetAll();
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
--- MusicOrganizer/Models/Record.cs
+++ MusicOrganizer/Models/Record.cs
@@ -5,9 +5,21 @@ namespace MusicOrganizer.Models
   public class Record
   {
     public string AlbumTitle { get; }
+    private static List<Record> _instances = new List<Record> {};
     public Record(string albumTitle)
     {
       AlbumTitle = albumTitle;
+      _instances.Add(this);
+    }
+
+    public static List<Record> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
