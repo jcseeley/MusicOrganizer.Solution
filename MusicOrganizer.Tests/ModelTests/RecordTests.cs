1418b40ba3258afb9fc566780c52cd1c937b6b76
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-03 11:41:34 -0800

    write and pass first test for RecordConstructor.

--- MusicOrganizer.Tests/ModelTests/RecordTests.cs
+++ MusicOrganizer.Tests/ModelTests/RecordTests.cs
@@ -6,78 +6,14 @@ using MusicOrganizer.Models;
 namespace MusicOrganizer.Tests
 {
   [TestClass]
-  public class RecordTests : IDisposable
+  public class RecordTests
   {
-    public void Dispose()
-    {
-      Class.ClearAll();
-    }
     
     [TestMethod]
-    public void ClassConstructor_CreatesInstanceOfClass_Object()
+    public void RecordConstructor_CreatesInstanceOfRecord_Object()
     {
-      Class newObject = new Class("test");
-      Assert.AreEqual(typeof(Class), newObject.GetType()); 
-    }
-
-    [TestMethod]
-    public void GetDescription_ReturnsDescription_String()
-    {
-      string description = "Walk the dog.";
-      Item newItem = new Item(description);
-      string result = newItem.Description;
-      Assert.AreEqual(description, result); 
-    }
-
-    [TestMethod]
-    public void SetDescription_SetDescription_String()
-    {
-      string description = "Walk the dog.";
-      Item newItem = new Item(description);
-      string updatedDescription = "Do the dishes.";
-      newItem.Description = updatedDescription;
-      string result = newItem.Description;
-      Assert.AreEqual(updatedDescription, result); 
-    }
-
-    [TestMethod]
-    public void GetAll_ReturnsEmptyList_ItemList()
-    {
-      List<Item> newList = new List<Item> { };
-      List<Item> result = Item.GetAll();
-      CollectionAssert.AreEqual(newList, result);
-    }
-
-    [TestMethod]
-    public void GetAll_ReturnsItems_ItemList()
-    {
-      string description01 = "Walk the dog";
-      string description02 = "Wash the dishes";
-      Item newItem1 = new Item(description01);
-      Item newItem2 = new Item(description02);
-      List<Item> newList = new List<Item> { newItem1, newItem2 };
-      List<Item> result = Item.GetAll();
-      CollectionAssert.AreEqual(newList, result);
-    }
-
-    [TestMethod]
-    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
-    {
-      string description = "Walk the dog.";
-      Item newItem = new Item(description);
-      int result = newItem.Id;
-      Assert.AreEqual(1, result);
-    }
-
-    [TestMethod]
-    public void Find_ReturnsCorrectItem_Item()
-    {
-      string description01 = "Walk the dog";
-      string description02 = "Wash the dishes";
-      Item newItem1 = new Item(description01);
-      Item newItem2 = new Item(description02);
-      Item result = Item.Find(2);
-      Assert.AreEqual(newItem2, result);
+      Record newRecord = new Record();
+      Assert.AreEqual(typeof(Record), newRecord.GetType());
     }
   }
 }
--- MusicOrganizer/Controllers/RecordController.cs
+++ MusicOrganizer/Controllers/RecordController.cs
@@ -1,53 +1,53 @@
-using System.Collections.Generic;
-using System;
-using Microsoft.AspNetCore.Mvc;
-using MusicOrganizer.Models;
+// using System.Collections.Generic;
+// using System;
+// using Microsoft.AspNetCore.Mvc;
+// using MusicOrganizer.Models;
 
-namespace MusicOrganizer.Controllers
-{
-  public class RecordController : Controller
-  {
+// namespace MusicOrganizer.Controllers
+// {
+//   public class RecordController : Controller
+//   {
 
-    [HttpGet("/objectClass")]
-    public ActionResult Index()
-    {
-      List<Class> allClassObjects = Class.GetAll();
-      return View(allClassObjects);
-    }
+//     [HttpGet("/objectClass")]
+//     public ActionResult Index()
+//     {
+//       List<Class> allClassObjects = Class.GetAll();
+//       return View(allClassObjects);
+//     }
 
-    [HttpGet("/objectClass/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
+//     [HttpGet("/objectClass/new")]
+//     public ActionResult New()
+//     {
+//       return View();
+//     }
 
-    [HttpPost("/objectClass")]
-    public ActionResult Create(string classObjectName)
-    {
-      Class newClass = new Class(classObjectName);
-      return RedirectToAction("Index");
-    }
+//     [HttpPost("/objectClass")]
+//     public ActionResult Create(string classObjectName)
+//     {
+//       Class newClass = new Class(classObjectName);
+//       return RedirectToAction("Index");
+//     }
 
-    [HttpGet("/objectClass/{id}")]
-    public ActionResult Show(int id)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Class classObjectToShow = Class.Find(id);
-      return View(classObjectToShow);
-    }
+//     [HttpGet("/objectClass/{id}")]
+//     public ActionResult Show(int id)
+//     {
+//       Dictionary<string, object> model = new Dictionary<string, object>();
+//       Class classObjectToShow = Class.Find(id);
+//       return View(classObjectToShow);
+//     }
 
-    [HttpPost("/objectClass/{id}/edit")]
-    public ActionResult Edit(int id)
-    {
+//     [HttpPost("/objectClass/{id}/edit")]
+//     public ActionResult Edit(int id)
+//     {
 
-      return View();
-    }
+//       return View();
+//     }
 
-    [HttpPost("/classObjects/delete")]
-    public ActionResult Delete()
-    {
-      Class.ClearAll();
-      return View();
-    }
-  }
-}
+//     [HttpPost("/classObjects/delete")]
+//     public ActionResult Delete()
+//     {
+//       Class.ClearAll();
+//       return View();
+//     }
+//   }
+// }
